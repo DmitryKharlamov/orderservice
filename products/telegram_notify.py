c9a1459535ab7c9dc240c8db4f092174c50f46d7
Harlamov Dmitry <dmitry.kharlamov9706@gmail.com>
2025-06-17 17:30:00 +0500

    fix_bot

--- products/telegram_notify.py
+++ products/telegram_notify.py
@@ -1,25 +1,19 @@
-import requests
+import telebot
 from django.conf import settings
+from accounts.models import CustomUser
 
 def notify_telegram(user):
-    from accounts.models import CustomUser
     user = CustomUser.objects.get(pk=user.pk)
 
     if not user.telegram_id:
         print(f"❌ У пользователя {user.phone} нет telegram_id. Уведомление не отправлено.")
         return
 
+    bot = telebot.TeleBot(settings.BOT_TOKEN)
+    msg = "Вам пришёл новый заказ!"
 
-
-    if user.telegram_id:
-
-        msg = "Вам пришёл новый заказ!"
-        token = settings.BOT_TOKEN
-        url = f"https://api.telegram.org/bot{token}/sendMessage"
-        response = requests.post(url, data={
-            'chat_id': user.telegram_id,
-            'text': msg
-        })
-        print(f"[DEBUG] Telegram API ответ: {response.status_code} — {response.text}")
-
-
+    try:
+        bot.send_message(chat_id=user.telegram_id, text=msg)
+        print(f"✅ Уведомление отправлено пользователю {user.phone}")
+    except Exception as e:
+        print(f"❌ Ошибка при отправке сообщения: {e}")
