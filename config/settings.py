9125b71fc7548837add378c19fc43d358ab23b6d
Harlamov Dmitry <dmitry.kharlamov9706@gmail.com>
2025-06-17 22:57:27 +0500

    add_test

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-# Django REST Framework
     'rest_framework',
     'subscriptions',
     'products.apps.ProductsConfig',
